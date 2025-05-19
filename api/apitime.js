9380698d435a189962774fce7b14eedcea9bd614
Androgametop <androiidtop@gmail.com>
2025-05-19 02:19:00 -0700

    Update apitime.js

--- api/apitime.js
+++ api/apitime.js
@@ -1,4 +1,10 @@
 export default function handler(req, res) {
+  const apiKey = req.headers['x-api-key'];
+  const validApiKey = process.env.MY_SECRET_KEY;
+
+  if (apiKey !== validApiKey) {
+    return res.status(403).json({ error: 'دسترسی غیر مجاز' });
+  }
+
   const now = new Date();
   res.status(200).json({ dateTime: now.toISOString() });
-}
